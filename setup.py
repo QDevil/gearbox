991f209d4d5130d8f2404af7bf0c1ae9aab1ea57
Jens Peter Secher <jps@qdevil.com>
2021-11-04 15:54:02 +0100

    Depend on stopit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygears-gearbox',
-    version='0.0.1rc5',
+    version='0.0.1rc6',
     description='PyGears visualization GUI',
 
     # The project's main homepage.
@@ -16,7 +16,8 @@ setup(
     license='MIT',
     python_requires='>=3.6.0',
     install_requires=[
-        'pygears', 'pexpect', 'PySide2!=5.12.1', 'pygraphviz', 'pygments', 'python-xlib'
+        'pygears', 'pexpect', 'PySide2!=5.12.1', 'pygraphviz', 'pygments',
+        'python-xlib', 'stopit'
     ],
     packages=find_packages(exclude=['docs']),
     package_data={'': ['*.css', '*.png', '*.tcl', 'gtkwaverc']},
